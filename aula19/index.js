952358fb91bb2c995c2806a8b36f09615a830c5e
juliatangerino <julianatangerino2017@gmail.com>
2021-06-21 22:07:17 -0300

    Alteração na aula 19 e adição da aula 20 com HTML

--- aula19/index.js
+++ aula19/index.js
@@ -1,8 +1,20 @@
 /*
 Tipos de dados:
 
-- Primitivos (imutáveis) - String, number, boolean, undefined, null (bigint, symbol)
-- Referência (matável) - array, object, function
+- Primitivos (imutáveis) - String, number, boolean, undefined, null (bigint, symbol) - Valores copiados
+- Referência (mutável) - array, object, function - Passado por refêrencia, apontar para o mesmo lugar
 
 */
+
+const pessoa = {
+    nome: 'Juliana',
+    sobrenome: 'Barbosa',
+    idade: 18
+}
+
+const pessoa2 = {...pessoa} //Faço uma cópia do objeto pessoa, sem que os 2 estejam ligados no mesmo lugar de armazenamento
+
+console.log(pessoa2);
+
+
 
